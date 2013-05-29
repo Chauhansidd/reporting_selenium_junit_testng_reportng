4fec2293bd0192dd0e7d51c6e66eaf4bac170a6d
Waleed Bin Dawood <waleed.dawood@confiz.com>
2013-05-29 23:01:24 +0500

    first ever commit to this repo

---
+++ src/test/java/com/confiz/ExampleTest.java
@@ -0,0 +1,30 @@
+package com.confiz;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Waleed
+ * Date: 5/29/13
+ * Time: 8:53 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+public class ExampleTest {
+
+
+    @Test
+    public void testSampleGoogle() {
+
+
+        Assert.assertTrue(true, "Page title is: " );
+    }
+
+}
