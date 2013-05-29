2084480fc9b6a854db710e6920265c372bc764a3
unknown <waleed.dawood@confiz.com>
2013-05-30 02:28:04 +0500

    updates

--- src/main/java/com/confiz/Example.java
+++ src/main/java/com/confiz/Example.java
@@ -16,9 +16,7 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Example {
     public static void main(String[] args) {
-        // Create a new instance of the html unit driver
-        // Notice that the remainder of the code relies on the interface,
-        // not the implementation.
+
         WebDriver driver = new FirefoxDriver();
 
         // And now use this to visit Google
--- src/test/java/com/confiz/ExampleTest.java
+++ src/test/java/com/confiz/ExampleTest.java
@@ -22,9 +22,24 @@ public class ExampleTest {
 
     @Test
     public void testSampleGoogle() {
+        WebDriver driver = new FirefoxDriver();
+
+        // And now use this to visit Google
+        driver.get("http://www.google.com");
+
+        // Find the text input element by its name
+        WebElement element = driver.findElement(By.name("q"));
+
+        // Enter something to search for
+        element.sendKeys("Cheese!");
+
+        // Now submit the form. WebDriver will find the form for us from the element
+        element.submit();
+
+        // Check the title of the page
 
 
-        Assert.assertTrue(true, "Page title is: " );
+        Assert.assertTrue(true, "Page title is: " + driver.getTitle());
     }
 
 }
